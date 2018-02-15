0191bd76269fdc77b67de14a6000601f1844d6c5
KertM <kert.moor@gmail.com>
2018-02-15 09:52:47 +0200

    1

--- DataCollection2/Program.cs
+++ DataCollection2/Program.cs
@@ -4,12 +4,237 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DataCollection2
+namespace DataCollection
 {
     class Program
     {
         static void Main(string[] args)
         {
+            #region MyRegion
+
+            ////var cars = new List<string>();
+
+            ////var car1 = "BMW";
+            ////var car2 = "LADA";
+            ////var car3 = "HONDA";
+            ////var car4 = "LAMBORGHINI";
+            ////var car5 = "FORD";
+            ////var car6 = "SAAB";
+
+            ////cars.Add(car1);
+            ////cars.Add(car2);
+            ////cars.Add(car3);
+            ////cars.Add(car4);
+            ////cars.Add(car5);
+            ////cars.Add(car6);
+
+            ////foreach (var item in cars)
+            ////{
+            ////    Console.WriteLine(item);
+            ////}
+
+            ////var cars = new List<string> { "BMW", "Lada", "Honda", "Lamborghini", "Ford", "Saab" };
+            ////for (int index = 0; index < cars.Count; index++)
+            ////{
+            ////    Console.WriteLine(cars[index]);
+            ////}
+
+            //var humans1 = new List<Human>
+            //{
+            //    new Human(){Name = "Kalle", Age = 20},
+            //    new Human(){Name = "Malle", Age = 25},
+            //    new Human(){Name = "Mari", Age = 14},
+            //    new Human(){Name = "Elmar", Age = 16},
+            //    new Human(){Name = "Juku", Age = 35},
+            //};
+
+            //var human1 = new Human();
+            //human1.Name = "Priit";
+            //human1.Age = 25;
+            //humans1.Add(human1);
+
+            //var human2 = new Human();
+            //Console.WriteLine("sisestage nimi");
+            //human2.Name = Console.ReadLine();
+            //Console.WriteLine("sisestage vanus");
+            //human2.Age = int.Parse(Console.ReadLine());
+            //humans1.Add(human2);
+
+            //foreach (var item in humans1)
+            //{
+            //    Console.WriteLine(item.Name + " " + item.Age);
+            //} 
+            #endregion
+
+            #region Dictionary
+            ////Dictionary
+
+            //var openWith = new Dictionary<string, string>();
+            ////            key       value
+            //openWith.Add("Txt", "notepad.exe");
+            //openWith.Add("bmp", "paint.exe");
+            //openWith.Add("jpg", "paint.exe");
+            //openWith.Add("rtf", "wordpad.exe");
+
+            //openWith["rtf"] = "winword.exe";
+            //openWith["doc"] = "winword.exe";
+
+            //try
+            //{
+            //    openWith.Add("Txt", "word.exe");
+            //}
+            //catch (ArgumentException)
+            //{
+            //    Console.WriteLine("An element with key \"txt\" already exists");
+            //}
+
+            //Console.WriteLine("For key = rtf, value = {0}", openWith["rtf"]);
+            //Console.WriteLine("For key = doc, value = {0}", openWith["doc"]);
+
+            //try
+            //{
+            //    Console.WriteLine("For key = tif, value = {0}", openWith["rtf"]);
+
+            //}
+            //catch (KeyNotFoundException)
+            //{
+            //    Console.WriteLine("Key = \"tif\" is not found");
+            //}
+
+            //string value = "";
+            //if (openWith.TryGetValue("tif", out value))
+            //{
+            //    Console.WriteLine("For key = \"tif\", value = {0}.");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Key = \"tif\" is not found");
+            //}
+
+            ////vaatame kas "ht" võti on olemas, kui ei ole lisame
+            //if (!openWith.ContainsKey("ht"))
+            //{
+            //    openWith.Add("ht", "hyperterm.exe");
+            //    Console.WriteLine("Value added for key 'ht': {0}", openWith["ht"]);
+            //}
+
+            ////Kpsime väärtused kasutades foreach tsüklis
+            //foreach (KeyValuePair<string, string> kvp in openWith)
+            //{
+            //    Console.WriteLine("Key = {0}, Value = {1}", kvp.Key, kvp.Value);
+            //}
+
+            ////Küsime väärtused
+            //Dictionary<string, string>.ValueCollection valueCollection = openWith.Values;
+            //foreach (string item in valueCollection)
+            //{
+            //    Console.WriteLine("Key = {0}", item);
+            //}
+
+            ////Küsime võtmed
+            //Dictionary<string, string>.KeyCollection keyCollection = openWith.Keys;
+            //foreach (var item in keyCollection)
+            //{
+            //    Console.WriteLine("Key = {0}", item);
+            //}
+
+            ////Kustutame võtme/väärtuse
+            //openWith.Remove("doc");
+            //if (!openWith.ContainsKey("doc"))
+            //{
+            //    Console.WriteLine("Key \"doc\" is not found");
+            //}
+            #endregion
+
+            #region Queue - First in first out (FIFO)
+
+            //Queue<string> numbers = new Queue<string>();
+            //numbers.Enqueue("one");
+            //numbers.Enqueue("two");
+            //numbers.Enqueue("three");
+            //numbers.Enqueue("four");
+            //numbers.Enqueue("five");
+
+            //foreach (string number in numbers)
+            //{
+            //    Console.WriteLine(number);
+            //}
+
+            //Console.WriteLine();
+
+            ////Võtame välja esimese elemendi
+            //Console.WriteLine("Dequeuing '{0}'", numbers.Dequeue());
+            ////Piilume järgmist elementi
+            //Console.WriteLine("Peek at next item to dequeue: '{0}'", numbers.Peek());
+            ////Võtame välja järgmise
+            //Console.WriteLine("Dequeuing '{0}'", numbers.Dequeue());
+
+            ////Teeme koopia järjekorrast
+            //Queue<string> queueCopy = new Queue<string>(numbers.ToArray());
+            //Console.WriteLine("Copy(1):");
+            //foreach (string number in queueCopy)
+            //{
+            //    Console.WriteLine(number);
+            //}
+
+            //string[] array2 = new string[numbers.Count * 2];
+            //numbers.CopyTo(array2, numbers.Count);
+
+            //Queue<string> queueCopy2 = new Queue<string>(array2);
+            //Console.WriteLine("\n Contents of the second Copy, with duplicates and nulls.");
+            //foreach (string number in queueCopy2)
+            //{
+            //    Console.WriteLine(number);
+            //}
+
+            //Console.WriteLine("\nqueueCopy contains 'four' = {0}", queueCopy.Contains("four"));
+            ////tühjendame järjekorra
+            //queueCopy.Clear();
+            //Console.WriteLine("\nqueueCopy count is {0} ", queueCopy.Count);
+
+
+            #endregion
+
+            #region Stack - Last in first out
+
+            //Stack<string> numbers = new Stack<string>();
+            //numbers.Push("one");
+            //numbers.Push("two");
+            //numbers.Push("three");
+            //numbers.Push("four");
+            //numbers.Push("five");
+
+            //foreach (string number in numbers)
+            //{
+            //    Console.WriteLine(number);
+            //}
+
+            //Console.WriteLine("\nPopping '{0}'", numbers.Pop());
+            //Console.WriteLine("Peek at newxt item to destack: {0}", numbers.Peek());
+            //Console.WriteLine("Popping '{0}'", numbers.Pop());
+
+
+
+
+
+            #endregion
+
+            #region LINQ
+
+            //Andmekogum    
+            int[] numbers = new int[7] {0, 1, 2, 3, 4, 5, 6 };
+
+            //Loome Päringu
+            var evenNumQuery = (from num in numbers
+                           where (num % 2) == 0
+                           select num).Tolist();
+                           
+            int evenNumCount = evenNumQuery.Count();
+            
+
+
+                           #endregion
+            Console.ReadLine();
         }
     }
 }
